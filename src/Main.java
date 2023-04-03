20db39eb8b89273acbbf40dd4bcc0c1730d3be09
ByCooper <kup.official@yahoo.com>
2023-04-03 21:01:28 +0300

    Домашнее задание 10(выполнено)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//------------------------------------------Задача 1
+        System.out.println("\n" + "Задача №1" + "\n");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = "Ivanov Ivan Ivanovich";
+
+        System.out.println("ФИО сотрудника - " + middleName + " " + firstName + " " + lastName);
+
+//-------------------------------------------Задача 2
+        System.out.println("\n" + "Задача №2" + "\n");
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+//-------------------------------------------Задача 3
+        System.out.println("\n" + "Задача №3" + "\n");
+
+        String fullName1 = "Иванов Семён Семёнович";
+
+        System.out.println("Данные ФИО сотрудника — " + fullName1.replace('ё', 'е'));
     }
 }
